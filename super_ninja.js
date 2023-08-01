90536da94fb32219d743a54a031a2d1afc381a9d
Anthonylooby1 <anthonylooby@yahoo.com>
2023-08-01 14:25:46 -0400

    Super Ninja

---
+++ super_ninja.js
@@ -0,0 +1,50 @@
+class Ninja {
+    constructor(name,health = 9, speed = 3, strength = 3) {
+        this.name = name;
+        this.health = health;
+        this.speed = speed;
+        this.strength = strength;
+
+    }
+
+    sayName() {
+        console.log(`Ninja name is ${this.name}`)
+        return;
+    }
+
+    showStats() {
+        console.log(`Ninja stats are ${this.name}`)
+        console.log(`Ninja stats are ${this.health}`)
+        console.log(`Ninja stats are ${this.speed}`)
+        console.log(`Ninja stats are ${this.strength}`)
+        return;
+    }
+
+    drinkSake() {
+        this.health += 10;
+        console.log(`health is now ${this.health}`)
+        return;
+    }
+}
+const ninja1 = new Ninja("Hyabusa");
+ninja1.sayName();
+ninja1.showStats();
+ninja1.drinkSake();
+
+class Sensei extends Ninja {
+    constructor (name) {
+        super(name, 200, 10, 10)
+        // this.health = 200
+        // this.speed = 10
+        // this.strength = 10
+        this.wisdom = 10
+    }
+
+    speakWisdom() {
+        console.log("What one programmer can do in one month, two programmers can do in two months")
+    }
+}
+const sensei1 = new Sensei('Tod')
+console.log(sensei1)
+sensei1.speakWisdom();
+sensei1.showStats();
